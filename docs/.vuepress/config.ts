c80ca7470b1c2643c70e467b98021a3ba6e908e5
Isha <ishadijcks@gmail.com>
2021-10-05 23:08:41 +0200

    Add demo

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -19,49 +19,59 @@ export default defineUserConfig<DefaultThemeOptions>({
             },
         ],
         sidebarDepth: 0,
-        sidebar: [
-            {
-                text: 'Introduction',
-                link: '/introduction/',
-                children: [
-                    '/docs/introduction/introduction.md',
-                    '/docs/introduction/project-structure.md',
-                    '/docs/introduction/launch-behaviour.md',
-                    '/docs/introduction/game-states.md',
-                ],
-            },
-            {
-                text: 'Features',
-                link: '/features/',
-                children: [
-                    '/docs/features/wallet.md',
-                    '/docs/features/settings.md',
-                    '/docs/features/statistics.md',
-                    '/docs/features/special-events.md',
-                    '/docs/features/achievements.md',
-                    '/docs/features/redeemable-codes.md',
-                    '/docs/features/key-items.md',
-                    '/docs/features/inventory.md',
-                    '/docs/features/item-list.md',
-                ],
-            },
-            {
-                text: 'Tools',
-                link: '/tools/',
-                children: [
-                    '/docs/tools/developer-panel.md',
-                    '/docs/tools/requirements.md',
-                    '/docs/tools/exp-level.md',
-                    '/docs/tools/boosters.md',
-                    '/docs/tools/loot-tables.md',
-                    '/docs/tools/hotkeys.md',
-                    '/docs/tools/probability.md',
-                    '/docs/tools/actions.md',
-                    '/docs/tools/items.md',
-                ],
-            },
+        sidebar: {
+            '/demo': [
+                {
+                    text: 'Introduction',
+                    children: [
+                        '/demo/introduction.md',
+                        '/demo/setting-up.md',
+                        '/demo/project-structure.md',
+                        '/demo/first-changes.md',
+                    ],
+                },
+            ],
+            '/docs': [
+                {
+                    text: 'Introduction',
+                    children: [
+                        '/docs/introduction/introduction.md',
+                        '/docs/introduction/project-structure.md',
+                        '/docs/introduction/launch-behaviour.md',
+                        '/docs/introduction/game-states.md',
+                    ],
+                },
+                {
+                    text: 'Features',
+                    children: [
+                        '/docs/features/wallet.md',
+                        '/docs/features/settings.md',
+                        '/docs/features/statistics.md',
+                        '/docs/features/special-events.md',
+                        '/docs/features/achievements.md',
+                        '/docs/features/redeemable-codes.md',
+                        '/docs/features/key-items.md',
+                        '/docs/features/inventory.md',
+                        '/docs/features/item-list.md',
+                    ],
+                },
+                {
+                    text: 'Tools',
+                    children: [
+                        '/docs/tools/developer-panel.md',
+                        '/docs/tools/requirements.md',
+                        '/docs/tools/exp-level.md',
+                        '/docs/tools/boosters.md',
+                        '/docs/tools/loot-tables.md',
+                        '/docs/tools/hotkeys.md',
+                        '/docs/tools/probability.md',
+                        '/docs/tools/actions.md',
+                        '/docs/tools/items.md',
+                    ],
+                },
 
-        ],
+            ],
+        }
     },
 
 })
---
+++ docs/demo/first-changes.md
@@ -0,0 +1,7 @@
+---
+title: First Changes
+---
+
+We will make 2 small changes now.
+You might have already noticed there are some TODO's in `src/my-game/MyGame.ts`
+
